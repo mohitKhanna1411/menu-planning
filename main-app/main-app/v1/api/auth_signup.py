07611e4b44152d3ebd6c186ac6dd2e1fcac50b36
Mohit Khanna <khanna.mohit94@gmail.com>
2021-07-30 12:51:20 +1000

    auth in

--- main-app/main-app/v1/api/auth_signup.py
+++ main-app/main-app/v1/api/auth_signup.py
@@ -1,15 +1,29 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, print_function
-
-from flask import request, g
-
-from . import Resource
 from .. import schemas
+from . import Resource
+from ..models import Users
+from ..helpers import gen_token
+from flask import request, g
+from werkzeug.security import generate_password_hash
+import uuid
 
 
 class AuthSignup(Resource):
 
     def post(self):
-        print(g.json)
+        data = request.get_json()
+        if not data or not data.get('password') or not data.get('username'):
+            return {'message': 'Username/Password Cannot be empty'}, 400, None
 
-        return {}, 200, None
+        user = Users.select().where(Users.username == data.get('username'))
+        if not user:
+            Users.create(
+                uuid=str(uuid.uuid4()),
+                username=data.get('username'),
+                password=generate_password_hash(data.get('password'))
+            )
+            token = gen_token(str(uuid.uuid4()))
+            return {'token': token}, 200, None
+        else:
+            return {'message': 'Username Taken'}, 409, None
