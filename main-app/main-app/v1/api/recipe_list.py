c85a7aa824751a0a21d7416a1665885d119acf95
Mohit Khanna <khanna.mohit94@gmail.com>
2021-07-31 14:04:02 +1000

    list recipe and ingredients

--- main-app/main-app/v1/api/recipe_list.py
+++ main-app/main-app/v1/api/recipe_list.py
@@ -2,6 +2,9 @@
 from __future__ import absolute_import, print_function
 
 from flask import request, g
+from ..models import Ingredients as In
+from ..models import Recipes as Re
+from ..helpers import authorize
 
 from . import Resource
 from .. import schemas
@@ -11,6 +14,33 @@ class RecipeList(Resource):
 
     def get(self):
         print(g.headers)
-        print(g.args)
+        authorize(g.headers)
 
-        return [], 200, None
+        recepie = Re.select().order_by(Re.id)
+        r_res = []
+        for r in recepie:
+            in_res = []
+            print("r-----", flush=True)
+            print(r.id, flush=True)
+            ingredients = In.select().where(In.recipe_id == r.id)
+            for i in ingredients:
+                print("i-----", flush=True)
+                print(i.name, flush=True)
+                in_res.append({
+                    "name": i.name,
+                    "image":i.image
+                })
+            r_res.append({
+                'id': r.id,
+                'uuid': r.uuid,
+                'name': r.name,
+                'created_at': r.created_at,
+                'classification': r.classification,
+                'image': r.image,
+                'nutirtional_information': r.nutirtional_information,
+                'ingredients': in_res,
+            })
+
+        # print(recepie)
+
+        return r_res, 200, None
